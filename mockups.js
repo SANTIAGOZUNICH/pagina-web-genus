bbc34b36e2b14ee3013fb707b787cd3c86320301
SANTIAGOZUNICH <szunich@uade.edu.ar>
2026-06-23 10:07:55 -0300

    Update mockups.js

--- mockups.js
+++ mockups.js
@@ -216,22 +216,21 @@ ${renderLabelText(90, 203, nombreProducto, {maxWidth:68, fontSize:15, letterSpac
 <line x1="56" y1="210" x2="124" y2="210" stroke="#aaa" stroke-width=".75"/>
 ${renderLabelText(90, 225, marcaLabel, {maxWidth:68, fontSize:14, letterSpacing:2, fill:'#666', minFontSize:8})}
 
-<!-- TETINA BLANCA -->
+<!-- TETINA NEGRA -->
 <ellipse cx="90" cy="60" rx="14" ry="22"
-         fill="#f8f8f8"/>
-         
-<ellipse cx="86" cy="50" rx="3" ry="6"
-         fill="black"
-         opacity=".8"/>
+         fill="#171717"/>
 
-<ellipse cx="90" cy="73" rx="8" ry="2"
-         fill="rgba(0,0,0,.10)"/>
+<ellipse cx="86" cy="50" rx="3" ry="6"
+         fill="rgba(255,255,255,.20)"/>
+
+<ellipse cx="90" cy="77" rx="9" ry="2"
+         fill="rgba(255,255,255,.10)"/>
          
 <!-- COLLAR / CAP ASSEMBLY -->
 <rect x="72" y="97" width="36" height="8" rx="4" fill="url(#sr-cap-${size})"/>
 <rect x="72" y="97" width="36" height="8" rx="4" fill="url(#sr-cap-v-${size})"/>
-<rect x="70" y="80" width="40" height="20" rx="7" fill="url(#sr-cap-${size})"/>
-<rect x="70" y="80" width="40" height="20" rx="7" fill="url(#sr-cap-v-${size})"/>
+<rect x="70" y="84" width="40" height="20" rx="7" fill="url(#sr-cap-${size})"/>
+<rect x="70" y="84" width="40" height="20" rx="7" fill="url(#sr-cap-v-${size})"/>
 <rect x="70" y="80" width="40" height="3"  rx="1.5" fill="rgba(255,255,255,.26)"/>
 <rect x="70" y="87" width="40" height="1"  rx=".5"  fill="rgba(255,255,255,.12)"/>
 <rect x="70" y="97" width="40" height="2"  rx="1"   fill="rgba(30,50,65,.26)"/>
